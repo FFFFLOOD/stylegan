ff9b3d69826139c5386d1a71671b547e2e7ef331
FLOOD <43155239+FFFFLOOD@users.noreply.github.com>
2019-11-14 15:09:50 -0500

    compat issues

--- dnnlib/tflib/tfutil.py
+++ dnnlib/tflib/tfutil.py
@@ -197,7 +197,7 @@ def set_vars(var_to_value_dict: dict) -> None:
         except KeyError:
             with absolute_name_scope(var.name.split(":")[0]):
                 with tf.control_dependencies(None):  # ignore surrounding control_dependencies
-                    setter = tf.compat.v1.assign(var, tf.placeholder(var.dtype, var.shape, "new_value"), name="setter")  # create new setter
+                    setter = tf.compat.v1.assign(var, tf.compat.v1.placeholder(var.dtype, var.shape, "new_value"), name="setter")  # create new setter
 
         ops.append(setter)
         feed_dict[setter.op.inputs[1]] = value
